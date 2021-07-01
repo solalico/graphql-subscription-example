d556613fd664558668cf7a5c76fadba6b3c60530
Di Zhang <Di.Zhang1@ibm.com>
2021-07-02 04:51:23 +0800

    with proxy

--- src/App.js
+++ src/App.js
@@ -6,41 +6,80 @@ import {execute, gql} from '@apollo/client'
 import {SubscriptionClient} from 'subscriptions-transport-ws'
 import {WebSocketLink} from '@apollo/client/link/ws'
 
+const token = ``
+
 function App() {
   const [link, setLink] = React.useState(null)
+  const [d, setData] = React.useState([])
+
   function startSub() {
-    const client = new SubscriptionClient(url, {
-      reconnect: false,
-      connectionCallback: (err) => {
-        // default: subscription by payload
-        const wsLink = new WebSocketLink(client)
-        execute(wsLink, {
-          query: gql`
-          subscription {
-            liveQuotes(stock: "IBM") {
-              lastPrice
+    let acked = 0
+    let dataArr = []
+    const wb = new WebSocket('ws://localhost:3020', 'graphql-ws')
+    setLink(wb)
+    wb.onopen = (e) => {
+      console.log('opened')
+      wb.send(JSON.stringify({ type: 'connect', url: '', token: token.trim(), headers: { 'x-ibm-client-id': '' } }))
+    }
+    wb.onmessage= (e) => {
+      const data = JSON.parse(e.data)
+      dataArr.push(e.data)
+      console.log('dataArr', dataArr)
+      setData([...dataArr])
+      if(data.type === 'connection_ack') {
+        if(acked === 0) {
+          wb.send(JSON.stringify({
+            type: "connection_init",
+            payload: {}
+          }))
+          acked++
+        } else if(acked === 1) {
+          wb.send(JSON.stringify({
+            type: 'start',
+            payload: {
+              query: `subscription {
+                liveQuotes(stock: "IBM") {
+                  lastPrice
+                }
+              }`
             }
-          }
-          `
-        }).subscribe((obj) => {
-          console.log('1', obj)
-        })
+          }))
+          acked++
+        }
       }
-    })
-    setLink(client)
+    }
+    // const client = new SubscriptionClient('ws://localhost:3020', {
+    //   reconnect: false,
+    //   connectionCallback: (err) => {
+    //     // default: subscription by payload
+    //     const wsLink = new WebSocketLink(client)
+    //     execute(wsLink, {
+    //       query: gql`
+    //       subscription {
+    //         liveQuotes(stock: "IBM") {
+    //           lastPrice
+    //         }
+    //       }
+    //       `
+    //     }).subscribe((obj) => {
+    //       console.log('1', obj)
+    //     })
+    //   }
+    // })
+    // setLink(client)
   }
 
   function stop() {
     link.close()
+    setData([])
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="display-panel">
+          {d}
+        </div>
         <input type='button' value='start' onClick={() => startSub()}/>
         <input type='button' value='stop' onClick={() => stop()}/>
         <a
